e3998f1ec40cf60e0726fcbcf71a8292dead177d
Tom Kocmi <kocmitom@gmail.com>
2017-06-15 16:15:38 +0200

    polishing up vocabulary class

--- neuralmonkey/vocabulary.py
+++ neuralmonkey/vocabulary.py
@@ -1,6 +1,7 @@
 """This module implements the Vocabulary class and the helper functions that
 can be used to obtain a Vocabulary instance.
 """
+# pylint: disable=too-many-lines
 
 import collections
 import os
@@ -508,8 +509,9 @@ class Vocabulary(collections.Sized):
         Arguments:
             path: The path to save the file to.
             overwrite: Flag whether to overwrite existing file.
-                       Defaults to False.
-            save_frequencies: store also frequencies?
+                Defaults to False.
+            save_frequencies: flag if frequencies should be stored. This
+                parameter adds header into the output file.
         Raises:
             FileExistsError if the file exists and overwrite flag is
             disabled.
@@ -527,7 +529,7 @@ class Vocabulary(collections.Sized):
                 # frequencies it MUST NOT contain the header. It is an
                 # exception from Tensorboard. More at
                 # https://www.tensorflow.org/get_started/embedding_viz
-                output_file.write("Word\tFrequency\n")
+                output_file.write("Word\tWord counts\n")
             elif save_frequencies and not self.correct_counts:
                 log("Storing vocabulary without frequencies.")
 
