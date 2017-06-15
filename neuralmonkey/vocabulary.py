bd593a435720f6f087cf07264def3802d543b9da
Tom Kocmi <kocmitom@gmail.com>
2017-06-15 17:06:42 +0200

    reviewers requests

--- neuralmonkey/vocabulary.py
+++ neuralmonkey/vocabulary.py
@@ -1,3 +1,4 @@
+# pylint: disable=too-many-lines
 """This module implements the Vocabulary class and the helper functions that
 can be used to obtain a Vocabulary instance.
 """
@@ -508,8 +509,9 @@ class Vocabulary(collections.Sized):
         Arguments:
             path: The path to save the file to.
             overwrite: Flag whether to overwrite existing file.
-                       Defaults to False.
-            save_frequencies: store also frequencies?
+                Defaults to False.
+            save_frequencies: flag if frequencies should be stored. This
+                parameter adds header into the output file.
         Raises:
             FileExistsError if the file exists and overwrite flag is
             disabled.
